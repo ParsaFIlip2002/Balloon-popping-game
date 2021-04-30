f809885652ef78f52f6d997cfe5105655606ad90
ParsaFIlip2002 <54800795+ParsaFIlip2002@users.noreply.github.com>
2021-04-30 17:56:46 +0200

    Lets gooo

--- Balloon popping game/MainWindow.xaml.cs
+++ Balloon popping game/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Balloon_popping_game
 {
@@ -20,9 +21,64 @@ namespace Balloon_popping_game
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+        int speed = 3;
+        int intervals = 90;
+        Random rand = new Random();
+
+        List<Rectangle> itemRemover = new List<Rectangle>();
+
+        ImageBrush backgroundImage = new ImageBrush();
+
+        int balloonSkins;
+        int missedBalloons;
+
+        bool gameIsActive;
+
+        int score;
+
+
+        MediaPlayer player = new MediaPlayer();
+
         public MainWindow()
         {
             InitializeComponent();
+            gameTimer.Tick += GameEnigne;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+
+            backgroundImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/background.image.jpg"));
+            MyCanvas.Background = backgroundImage;
+
+            RestartGame();            
         }
+
+        private void GameEnigne(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void PopBalloons(object sender, MouseButtonEventArgs e)
+        {
+
+     
+        }
+        private void StartGame ()
+        {
+
+        }
+        private void RestartGame()
+        {
+
+            foreach (var x in MyCanvas.Children.OfType<Rectangle>())
+            {
+                itemRemover.Add(x);
+            }
+            foreach ( Rectangle y in itemRemover)
+            {
+                MyCanvas.Children.Remove(y);            }
+        }
+
     }
 }
